34d4c40da97b706e57438cf9e68d189d9c0bf78e
dantan123 <dtan9988@gmail.com>
2021-01-13 23:59:46 +0000

    handle user input

--- play.js
+++ play.js
@@ -4,3 +4,29 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Set up user interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+ const setupInput = function() {
+   const stdin = process.stdin;
+   stdin.setRawMode(true);
+   stdin.setEncoding('utf-8');
+   stdin.resume();
+
+   stdin.on('data', key => {
+     handleUserInput(key);
+   });
+
+   return stdin;
+ };
+
+ const handleUserInput = function(data) {
+   if (data === '\u0003') {
+     process.exit();
+   }
+ };
+
+ setupInput();
